5d03028dd6a7cb970eb1fa7f0d7ee7d0e331718e
Diwas KC <diwaskc12@gmail.com>
2025-02-26 11:12:02 +0545

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  
+  bool exit = false;
 
   // Uncomment this block to pass the first stage
   do {
@@ -12,7 +14,24 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
-    std::cerr << input << ": command not found" << "\n";
+    int limiterIndex = input.find(" "); 
+    int cutoffLength = limiterIndex;
+    if (limiterIndex == -1) {
+      cutoffLength = input.size();
+      limiterIndex = input.size();
+    } else {
+      limiterIndex++;
+    }
+
+    std::string cmd = input.substr(0,cutoffLength);
+    std::string args = input.substr(limiterIndex, input.size());    
+    if (cmd == "exit") {
+      if (args == "0") {
+        exit = true;
+      }
+    } else {
+        std::cerr << cmd << ": command not found" << "\n";
+    }
     
-  } while(true);
+  } while(exit == false);
 }
